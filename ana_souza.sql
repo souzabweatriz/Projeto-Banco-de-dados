2327eaca39a1c312c1c9136c8800b7fe6e688160
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2024-12-03 07:42:07 -0300

    refactor: adjust name table

--- ana_souza.sql
+++ ana_souza.sql
@@ -4,7 +4,7 @@ CREATE DATABASE sitefisica;
 
 /*Criação da tabela de Cadastro dos estudantes*/
 CREATE TABLE cadastro_estudantes (
-    id_estudante SERIAL PRMARY KEY,
+    id_estudante SERIAL PRIMARY KEY,
     nome VARCHAR(70) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(100) NOT NULL
